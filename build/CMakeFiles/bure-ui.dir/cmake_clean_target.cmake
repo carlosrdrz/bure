53794ca3cbdcf3b7e215b583923207a7a01799b7
Carlos Rodriguez <crresse@gmail.com>
2018-07-19 01:20:33 +0200

    Removed build files

--- build/CMakeFiles/bure-ui.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libbure-ui.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carlosrdrz/dev/bure/client
-# Build directory: /home/carlosrdrz/dev/bure/client/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
